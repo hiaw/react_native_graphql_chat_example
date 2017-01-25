66bcb6a608085031bea0e1d72372b9cab89c9ef1
Daniel Chong <danielgq.chong@gmail.com>
2017-01-25 13:09:31 +1300

    able to see message view

---
+++ App/Components/MessageView.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+
+export default class MessgeView extends Component {
+  render () {
+    return (
+      <Text style={styles.text} >
+        {this.props.content}
+      </Text>
+    )
+  }
+}
+
+const styles = {
+  text: {
+    marginVertical: 5
+  }
+}
--- App/Root.js
+++ App/Root.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import LoginContainer from './Components/LoginUserForm.js'
+import LoginContainer from './Components/MessageView.js'
 
 export default class Root extends Component {
   render () {
     return (
-      <LoginContainer />
+      <LoginContainer content='testing' />
     )
   }
 }
