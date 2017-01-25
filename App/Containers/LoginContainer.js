c286e61baec87e4b2b9fbe003596242994c8330c
Daniel Chong <danielgq.chong@gmail.com>
2017-01-25 14:40:36 +1300

    directly show the channel view when scaphold user id exist

--- App/Containers/LoginContainer.js
+++ App/Containers/LoginContainer.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
+import { observer } from 'mobx-react'
 
 import LoginUserForm from '../Components/LoginUserForm.js'
+import ChannelView from '../Components/ChannelView.js'
 
+import store from '../Model/MainStore.js'
+
+@observer
 export default class LoginContainer extends Component {
   render () {
-    return (
-      <LoginUserForm
-        loginUser={this.loginUser} />
-    )
+    if (store.userDevice.scaphold_user_id) {
+      return (
+        <ChannelView />
+      )
+    } else {
+      return (
+        <LoginUserForm />
+      )
+    }
   }
 }
