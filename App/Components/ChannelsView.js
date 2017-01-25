0e73915f560817985878f6c985fc3fa76e66653e
Daniel Chong <danielgq.chong@gmail.com>
2017-01-25 15:48:36 +1300

    able to show list view of channels

--- App/Components/ChannelsView.js
+++ App/Components/ChannelsView.js
@@ -1,38 +1,26 @@
 import React from 'react'
-import { ListView, View, Text, Button, AlertIOS } from 'react-native'
+import { View, Text, Button, AlertIOS } from 'react-native'
+import { List, ListItem } from 'react-native-elements'
 
 import createChannel from '../Auth/CreateChannel.js'
 
-export default class CommentsView extends React.Component {
-  constructor (props) {
-    super(props)
-
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
-    this.state = {
-      dataSource: ds.cloneWithRows(props.getUser.channels.edges)
-    }
-  }
-
+export default class ChannelsView extends React.Component {
   createChannel () {
     AlertIOS.prompt('Channel name', 'Give the new channel a name',
       text => createChannel(text)
     )
   }
 
-  _renderRow (rowData) {
-    let row = rowData.node
-    return (
-      <Text>{`${row.name} (${row.members.aggregations.count})`}</Text>
-    )
-  }
-
   render () {
     return (
       <View style={styles.container}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => this._renderRow(rowData)}
-        />
+        <List>
+          {
+            this.props.getUser.channels.edges.map((row, i) => (
+              <ListItem key={i} title={`${row.node.name} (${row.node.members.aggregations.count})`} />
+              ))
+          }
+        </List>
         <Button title='Create Channel' onPress={() => this.createChannel(true)} />
       </View>
     )
@@ -42,9 +30,6 @@ export default class CommentsView extends React.Component {
 const styles = {
   container: {
     flex: 1,
-    paddingTop: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: 'lightblue'
   }
 }
